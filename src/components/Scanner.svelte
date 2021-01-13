b6b922853c1513916ed33070bbf2605a8b963f30
moritzebeling <git@moritzebeling.com>
2021-01-13 23:32:03 +0100

    implement p5 camera capture

--- src/components/Scanner.svelte
+++ src/components/Scanner.svelte
@@ -1,11 +1,71 @@
 <script>
 
-    import Placeholder from './Placeholder.svelte';
+    import p5 from 'p5';
+
+    let capture;
+
+    let grid = {
+        columns: 3,
+        rows: 3,
+        w: 0,
+        h: 0
+    };
+
+    const s = ( sketch ) => {
+        sketch.setup = () => {
+            sketch.createCanvas(container.offsetWidth, container.offsetHeight);
+            sketch.frameRate( 3 );
+
+            capture = sketch.createCapture(sketch.VIDEO);
+            // capture.hide();
+            capture.size( grid.columns, grid.rows );
+
+            grid.w = sketch.width / grid.columns;
+            grid.h = sketch.height / grid.rows;
+
+            sketch.fill( 255, 0, 0 );
+            sketch.noStroke();
+
+        };
+        sketch.draw = () => {
+
+            for ( let y = 0; y < grid.rows; y++ ) {
+                for ( let x = 0; x < grid.columns; x++ ) {
+
+                    let p = capture.get( x, y );
+                    let c = Math.round( ( (p[0] + p[1] + p[2]) / 765 ) * 1.2 );
+
+                    sketch.fill( c * 255 );
+                    sketch.rect( grid.w * x, grid.h * y, grid.w + 1, grid.h + 1 );
+
+                }
+            }
+
+        };
+    };
+
+    let container;
+    let myp5;
+
+    import { onMount } from 'svelte';
+    onMount(()=>{
+        myp5 = new p5(s, container);
+    });
 
 </script>
 
-<Placeholder columns={4} rows={4} />
+<div bind:this={container}></div>
 
 <style lang="scss">
+
+    div {
+        width: 100%;
+        height: 100%;
+    }
+
+    :global( video ){
+        opacity: 0;
+        display: absolute;
+    }
 
 </style>
